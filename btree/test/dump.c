20b218528d499a8c6f3a93a1fbfcbec22dd86420
Yusuke Nomura <yunomu@gmail.com>
2011-12-16 20:21:16 +0900

    split dump() to dump.c

---
+++ btree/test/dump.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include <btree.h>
+
+static void
+dump_in(
+	addr_t addr,
+	int d
+)
+{
+	struct bt_node *node = (struct bt_node *) addr;
+	int i;
+
+	printf("%d: ", d);
+	for (i = 0; i < node->size; i++) {
+		printf("%d ", node->entries[i].record);
+	}
+	printf("\n");
+
+	if (node->addr0 == ADDR_NULL) return;
+
+	dump_in(node->addr0, d+1);
+	for (i = 0; i < node->size; i++) {
+		dump_in(node->entries[i].addr, d+1);
+	}
+}
+
+void
+dump(BTREE* btree)
+{
+	dump_in(btree->root, 0);
+}
--- btree/test/test.c
+++ btree/test/test.c
@@ -3,35 +3,6 @@
 
 #include <btree.h>
 
-static void
-dump_in(
-	addr_t addr,
-	int d
-)
-{
-	struct bt_node *node = (struct bt_node *) addr;
-	int i;
-
-	printf("%d: ", d);
-	for (i = 0; i < node->size; i++) {
-		printf("%d ", node->entries[i].record);
-	}
-	printf("\n");
-
-	if (node->addr0 == ADDR_NULL) return;
-
-	dump_in(node->addr0, d+1);
-	for (i = 0; i < node->size; i++) {
-		dump_in(node->entries[i].addr, d+1);
-	}
-}
-
-void
-dump(BTREE* btree)
-{
-	dump_in(btree->root, 0);
-}
-
 BTREE* btree;
 
 void
@@ -54,7 +25,6 @@ as(int a[], int size)
 		store(btree, &(a[i]));
 	}
 }
-
 
 void
 test1()
