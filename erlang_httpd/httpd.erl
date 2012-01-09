2e050b1c4b34043e04da2a0088d3860b2f45eafa
Yusuke Nomura <yunomu@gmail.com>
2012-01-09 10:56:35 +0900

    add erlang_httpd

---
+++ erlang_httpd/httpd.erl
@@ -0,0 +1,35 @@
+-module(httpd).
+-export([start/0]).
+%-compile(export_all).
+
+start() -> start(31011).
+
+start(Port) ->
+	{ok, Listen} = gen_tcp:listen(Port,
+						[binary, {reuseaddr, true}, {active, true}]),
+	io:format("start httpd.~nport: ~p~n", [Port]),
+	loop(Listen),
+	gen_tcp:close(Listen).
+
+loop(Listen) ->
+	{ok, Socket} = gen_tcp:accept(Listen),
+	receive
+		{tcp, Socket, Bin} ->
+			Str = binary_to_list(Bin),
+			io:format(Str),
+			sock_write(Socket, "HTTP/1.0 200 OK\r\n"),
+			sock_write(Socket, "Content-type: text/html\r\n\r\n"),
+			sock_write(Socket, "<h1>Index</h1>"),
+			gen_tcp:close(Socket),
+			loop(Listen);
+		{tcp_closed, Socket} ->
+			io:format("Server socket closed~n"),
+			loop(Listen);
+		Any ->
+			io:format("Received: ~p~n", [Any]),
+			loop(Listen)
+	end.
+
+sock_write(Socket, Str) ->
+	gen_tcp:send(Socket, list_to_binary(Str)).
+
