d8723c95d5ce6218a1a6e6e28d01d79749d45d7c
Yusuke Nomura <yunomu@gmail.com>
2012-02-25 09:09:18 +0900

    caseを除去。$を使ってみた。他ちょっと。

--- dc_haskell/dc2.hs
+++ dc_haskell/dc2.hs
@@ -6,7 +6,7 @@ import Parser
 ope :: Stack Int -> (Int -> Int -> Int) -> Maybe (Stack Int)
 ope s f = do (v1, s1) <- pop s
              (v2, s2) <- pop s1
-             return (push s2 (f v1 v2))
+             return $ push s2 $ f v1 v2
 
 calcError :: Stack Int -> IO ()
 calcError s = do putStrLn "ERROR."
@@ -15,8 +15,11 @@ calcError s = do putStrLn "ERROR."
 display :: Stack Int -> String -> IO ()
 display s xs = maybe (calcError empty) display' (peek s)
                where
-                 display' = \v -> do (putStrLn.show) v
+                 display' = \v -> do putValLn v
                                      input s
+
+putValLn :: Show a => a -> IO ()
+putValLn = putStrLn . show
 
 quit :: IO ()
 quit = putStrLn "bye v(^n^)v"
@@ -29,12 +32,13 @@ clear = do putStrLn "clear."
            input empty
 
 readInt :: String -> Maybe Int
-readInt str = case parse int str of
-                [(v, "")] -> Just v
-                _         -> Nothing
+readInt str = f $ parse int str
+              where
+                f [(v, "")] = Just v
+                f _         = Nothing
 
 store :: Stack Int -> String -> IO ()
-store s xs = maybe (calcError s) (input.push s) (readInt xs)
+store s xs = maybe (calcError s) (input . push s) (readInt xs)
 
 proc :: Stack Int -> String -> IO ()
 proc s xs
@@ -48,11 +52,11 @@ proc s xs
     | xs == "/" = calc s div
     | otherwise = store s xs
 
-prompt :: IO ()
-prompt = putStr "> " >> hFlush stdout
+prompt :: String -> IO ()
+prompt p = putStr p >> hFlush stdout
 
 input :: Stack Int -> IO ()
-input s = do prompt
+input s = do prompt "> "
              xs <- getLine
              proc s xs
 
