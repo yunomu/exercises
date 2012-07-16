1f8ecf08dcf595582c440b0eb309041d680251fc
Yusuke Nomura <yunomu@gmail.com>
2012-07-16 15:41:38 +0900

    http-attoparsec

---
+++ http-attoparsec/Main.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE OverloadedStrings, FlexibleContexts #-}
+module Main where
+
+import Control.Applicative
+import Control.Concurrent
+import qualified Control.Exception.Lifted as E
+import Control.Failure
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Control
+import Control.Monad.Trans.Resource
+import Data.Attoparsec.ByteString
+import qualified Data.Attoparsec.ByteString.Char8 as AC
+import Data.ByteString
+import qualified Data.ByteString.Char8 as BC
+import Data.Conduit
+import Data.Conduit.Attoparsec
+import qualified Data.Conduit.Binary as CB
+import Network.HTTP.Conduit
+
+import Debug.Trace
+
+curl :: (MonadUnsafeIO m,
+         MonadThrow m,
+         MonadIO m,
+         MonadBaseControl IO m,
+         Failure HttpException m) =>
+    String -> m (ResumableSource (ResourceT m) ByteString)
+curl url = do
+    request <- parseUrl url
+    withManager $ \manager ->
+        responseBody <$> http request manager
+
+takeDigits :: (Monad m, MonadThrow m) => GLSink ByteString m ByteString
+takeDigits = sinkParser field
+
+field :: Parser ByteString
+field = AC.takeWhile (/= ',') <* AC.anyChar
+
+getIntList :: (MonadBaseControl IO m, MonadThrow m) =>
+    ResumableSource m ByteString ->
+    m [Int]
+getIntList src = E.handle handler $ do
+--getIntList src = do
+    (src1, str1) <- src $$++ takeDigits
+    let v = read $ BC.unpack str1
+    if v < 10
+      then getIntList src1
+      else (v:) <$> getIntList src1
+  where
+    handler :: (MonadBaseControl IO m) => ParseError -> m [a]
+    handler _ = return []
+
+printList :: (MonadIO m, MonadBaseControl IO m, MonadThrow m) =>
+    ResumableSource m ByteString -> m ()
+printList src = E.handle handler $ do
+    (src1, str1) <- src $$++ takeDigits
+    liftIO $ forkIO $ BC.putStrLn str1
+    printList src1
+  where
+    handler :: (MonadBaseControl IO m) => ParseError -> m ()
+    handler _ = return ()
+
+main :: IO ()
+main = runResourceT $ do
+    src0 <- liftIO $ curl "http://localhost:8080/"
+--    ss <- getIntList src0
+--    liftIO $ print ss
+    printList src0
+
