e1ca0fd568ea8f4c853aff2bd47fe66f86f0fb13
Yusuke Nomura <yunomu@gmail.com>
2012-09-17 22:45:55 +0900

    wait for

--- http-attoparsec/Main.hs
+++ http-attoparsec/Main.hs
@@ -13,8 +13,10 @@ import Data.ByteString
 import qualified Data.ByteString.Char8 as BC
 import Data.Conduit
 import qualified Data.Conduit.Binary as CB
+import qualified Data.Conduit.List as CL
 import Data.Conduit.Attoparsec
 import Network.HTTP.Conduit
+import Control.Concurrent.Event
 
 curl :: (MonadBaseControl IO m, MonadResource m) =>
     String -> m (ResumableSource m ByteString)
@@ -43,18 +45,31 @@ getIntList src = E.handle handler $ do
     handler _ = return []
 
 printList :: (MonadBaseControl IO m, MonadResource m)
-    => Sink ByteString m ()
-printList = do
-    str1 <- takeField
-    liftIO $ forkIO $ BC.putStrLn str1
-    printList
+    => ResumableSource m ByteString -> m ()
+printList src = printList' src []
+  where
+    printList' src0 es = do
+        (src1, field) <- src0 $$++ takeField
+        e <- liftIO $ fork $ BC.putStrLn field
+        let es' = e:es
+        E.handle (ignore es') $ printList' src1 es'
 
-ignore :: (MonadBaseControl IO m) => ParseError -> m ()
-ignore _ = return ()
+fork :: IO () -> IO Event
+fork proc = do
+    event <- new
+    forkIO $ withEvent event proc
+    return event
+
+withEvent :: Event -> IO a -> IO a
+withEvent event proc = E.finally proc $ set event
+
+ignore :: (MonadResource m, MonadBaseControl IO m)
+    => [Event] -> ParseError -> m ()
+ignore es _ = liftIO $ mapM_ wait es
 
 main :: IO ()
 main = runResourceT $ do
 --    src0 <- curl "http://localhost:8080/"
---    printList src0
-    E.handle ignore $ CB.sourceFile "index.html" $$ printList
+    (src0, _) <- CB.sourceFile "index.html" $$+ CL.take 0
+    printList src0
 
