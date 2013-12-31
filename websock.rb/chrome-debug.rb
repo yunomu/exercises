664a8d07fe30fef4035e0147553045e139bffb60
Yusuke Nomura <yunomu@gmail.com>
2013-12-31 12:34:25 +0900

    frappuccino

--- websock.rb/chrome-debug.rb
+++ websock.rb/chrome-debug.rb
@@ -1,62 +1,73 @@
 #!/usr/bin/ruby
 
-require 'rubygems'
 require 'websocket-client-simple'
 require 'json'
 require 'open-uri'
+require 'frappuccino'
 
 def pagelist(host = "localhost", port = 9222)
-    JSON.parse open("http://#{host}:#{port}/json") {|f|
-        f.read
-    }
+  JSON.parse open("http://#{host}:#{port}/json") {|f|
+    f.read
+  }
 end
 
-w = pagelist.find {|e| e["title"].include? "GitHub" }
-print w, $/
+w = pagelist.find {|e| e["title"].include? "frames" }
+puts w
 
 MAX = 2**64
 
 debugUrl = w["webSocketDebuggerUrl"] || begin
-    print "debugUrl is nil", $/
-    exit 1
+  puts "debugUrl is nil"
+  exit 1
 end
 
 def evaluate(expr)
-    {
-        "id" => Random.rand(MAX),
-        "method" => "Runtime.evaluate",
-        "params" => {
-            "expression" => expr,
-            "returnByValue" => true
-        }
+  {
+    "id" => Random.rand(MAX),
+    "method" => "Runtime.evaluate",
+    "params" => {
+      "expression" => expr,
+      "returnByValue" => true
     }
-end
-
-def timeline(op)
-    {
-        "id" => Random.rand(MAX),
-        "method" => "Timeline.#{op}"
-    }
+  }
 end
 
 ws = WebSocket::Client::Simple.connect debugUrl
 
+class Emitter
+  def send(data)
+    emit(data)
+  end
+end
+emitter = Emitter.new
+stream = Frappuccino::Stream.new(emitter)
+
+# debug
+#stream.on_value do |v|
+#  puts v
+#end
+
+stream
+  .select {|d| d["method"] == "Timeline.eventRecorded"}
+  .map {|d| puts d }
+
+stream
+  .select {|d| d["method"] != "Timeline.eventRecorded"}
+  .map {|d| open("log", "a+") {|f| f.puts d } }
+
 ws.on :message do |msg|
-    data = JSON.parse(msg.data)
-    next if data["method"] != "Timeline.eventRecorded"
-    print "response: ", data["params"], $/
+  data = JSON.parse(msg.data)
+  emitter.send(data)
 end
 
 ws.on :open do
-    dat = JSON.dump timeline("start")
-    ws.send dat
+  puts "open"
+  ws.send JSON.dump("id" => 1, "method" => "Timeline.start")
 end
 
 ws.on :close do |s|
-    print "close", $/
+  puts "close"
 end
 
 sleep 5
-ws.send JSON.dump(timeline("stop"))
-sleep 1
 ws.close
